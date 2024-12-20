6d1985fbb72a1c042e04b16e9c2632628ce1d11d
Sebastian <sormsebastian@gmail.com>
2024-12-20 23:16:02 +0100

    Counter complete
    Completed the counter. Pressing left alt will start a counter state that lasts 50ms, where if an attack hits it becomes countered. Currently, as a placeholder, the player is granted 1.5s of immunity.

--- 1. Other/3. Player/Scripts/player.gd
+++ 1. Other/3. Player/Scripts/player.gd
@@ -7,12 +7,7 @@ signal Attack3
 signal Attack4
 
 
-const DASH_SPEED = 1400.0
-const DASH_TIME = 0.2
-
-var invinc_timer = 1
-
-
+##### General variables #####
 @onready var player_sprite = $Sprite
 @onready var player_world_collision = $WorldCollision
 @onready var player_hurtbox_collision = $Hurtbox/HurtboxCollision
@@ -30,6 +25,15 @@ var suspend_movement_timer: float = 0.0
 @onready var bump_ray_left_lower = $Rays/BumpRayLeftLower
 @onready var bump_ray_right_lower = $Rays/BumpRayRightLower
 
+##### Dash variables #####
+var dash_countdown: float
+var dash_cooldown_countdown: float = 0
+var is_dashing: bool = false
+@export var dash_cooldown: float = 1.0
+@export var dash_time: float = 0.2
+@export var dash_distance: float = 280
+@onready var dash_velocity: float = dash_distance / dash_time
+
 ##### Jump variables #####
 var jump_counter: int = 0
 var jump_buffer_countdown: float
@@ -45,27 +49,20 @@ var coyote_countdown: float
 @onready var jump_gravity: float = 2.0 * jump_height / (jump_peak_time * jump_peak_time)
 @onready var descend_gravity: float = 2.0 * jump_height / (jump_descend_time * jump_descend_time)
 
-##### Dash variables #####
-var dash_countdown: float
-var dash_cooldown_countdown: float = 0
-var is_dashing: bool = false
-@export var dash_cooldown: float = 1.0
-@export var dash_time: float = 0.2
-@export var dash_distance: float = 280
-@onready var dash_velocity: float = dash_distance / dash_time
+##### Health variables #####
+var invinc_timer: float = 1.0
 
 ##### Attack variables #####
-@export var attack_damage: int = 10
-@export var punch_speed: float = 0.2  # Time for punch to complete
 var can_attack: bool = true
-var attack_active: bool = false
-var attack_timer: float = 0
 
-##### Parry/Counter variables #####
-@export var parry_window: float = 0.2  # Time window for successful parry
-@export var counter_window: float = 0.2  # Time window for successful counter
-var parry_active: bool = false
-var counter_active: bool = false
+##### Counter and parry variables #####
+var can_counter: bool = true
+var counter_cooldown_timer: float = 0.0
+var is_countering: bool = false
+var counter_active_timer: float = 0.0
+@export var counter_cooldown: float = 1.0
+@export var counter_duration: float = 0.05 # THIS MAY CHANGE DUE TO DIFFICULT BOSS ATTACK REACTION TIMES
+
 
 ##### Inventory Variables #####
 @export var inventory: Inventory
@@ -79,6 +76,7 @@ func _physics_process(delta):
 	handle_dash()
 	handle_movement(delta)
 	handle_facing_direction()
+	handle_attacks()
 	handle_counter()
 	move_and_slide()
 
@@ -87,81 +85,7 @@ func _physics_process(delta):
 	countdown_coyote(delta)
 	countdown_dash(delta)
 	handle_damage_timers(delta)
-
-	handle_attack(delta)
-	handle_parry_counter(delta)
-
-	
-	
-
-
-##### Jump functions #####
-func countdown_jump_buffer(delta): # Counts down the jump_buffer_countdown variable.
-	if Input.is_action_just_pressed("Jump"):
-		jump_buffer_countdown = jump_buffer
-	else:
-		jump_buffer_countdown -= delta
-
-
-func countdown_coyote(delta): # Counts down the coyote_countdown variable.
-	if is_on_floor():
-		coyote_countdown = coyote_time
-	else:
-		coyote_countdown -= delta
-
-
-func handle_jump(): # Responsible for jump and double jump mechanics.
-	if suspend_movement == false:
-		if is_on_floor():
-			jump_counter = 0
-			
-		if coyote_countdown > 0 and jump_buffer_countdown > 0:
-			velocity.y = jump_velocity
-			jump_buffer_countdown = 0
-		elif coyote_countdown < 0 and Input.is_action_just_pressed("Jump") and jump_counter < max_jumps - 1:
-			velocity.y = jump_velocity
-			jump_counter += 1
-			jump_buffer_countdown = 0
-			player_sprite.modulate = Color(0, 1, 0,) #Below 3 lines are to see when double jump occurs (as we dont have anim yet)
-			await get_tree().create_timer(0.3).timeout
-			player_sprite.modulate = Color(1, 1, 1)
-			
-		if not Input.is_action_pressed("Jump") and velocity.y < 0:
-			velocity.y = lerp(velocity.y, 0.0, 0.3)
-			
-		if Input.is_action_just_released("Jump"):
-			coyote_countdown = 0
-
-
-func handle_gravity(delta): # Controls gravities.
-	if velocity.y < 0:
-		velocity.y += jump_gravity * delta
-	elif velocity.y >= 0 and velocity.y < max_fall_speed:
-		velocity.y += descend_gravity * delta
-
-
-
-##### Dash functions #####
-func countdown_dash(delta): # Counts down the dash_countdown variable.
-	if Input.is_action_just_pressed("Dash") and is_dashing == false and dash_cooldown_countdown < 0:
-		dash_countdown = dash_time
-		is_dashing = true
-	else:
-		dash_countdown -= delta
-		dash_cooldown_countdown -= delta
-
-
-func handle_dash(): # Responsible for the dash mechanic.
-	if is_dashing == true and get_movement_direction() != 0:
-		velocity.x = dash_velocity * sign(get_movement_direction())
-		$AnimationPlayer.play("Run")
-	elif is_dashing == true and get_movement_direction() == 0:
-		velocity.x = dash_velocity * -sign(handle_facing_direction())
-		$AnimationPlayer.play("Run")
-	
-	if is_dashing == true and dash_countdown < 0:
-		is_dashing = false
-		dash_cooldown_countdown = dash_cooldown
+	handle_counter_cooldowns(delta)
 
 
 
@@ -252,24 +176,121 @@ func handle_movement(delta): # Responsible for movement left and right.
 
 
 
+##### Dash functions #####
+func countdown_dash(delta): # Counts down the dash_countdown variable.
+	if Input.is_action_just_pressed("Dash") and is_dashing == false and dash_cooldown_countdown < 0:
+		dash_countdown = dash_time
+		is_dashing = true
+	else:
+		dash_countdown -= delta
+		dash_cooldown_countdown -= delta
 
-func handle_counter(): # A very basic placeholder for the counter. A time between counters will later be added, dependent on whether it is succesful or not.
-	if Input.is_action_just_pressed("Counter"):
-		player_sprite.modulate = Color(1, 1, 1, 0.5)
-		await get_tree().create_timer(0.3).timeout
-		player_sprite.modulate = Color(1, 1, 1)
+
+func handle_dash(): # Responsible for the dash mechanic.
+	if is_dashing == true and get_movement_direction() != 0:
+		velocity.x = dash_velocity * sign(get_movement_direction())
+		$AnimationPlayer.play("Run")
+	elif is_dashing == true and get_movement_direction() == 0:
+		velocity.x = dash_velocity * -sign(handle_facing_direction())
+		$AnimationPlayer.play("Run")
+	
+	if is_dashing == true and dash_countdown < 0:
+		is_dashing = false
+		dash_cooldown_countdown = dash_cooldown
 
 
 
+##### Jump functions #####
+func countdown_jump_buffer(delta): # Counts down the jump_buffer_countdown variable.
+	if Input.is_action_just_pressed("Jump"):
+		jump_buffer_countdown = jump_buffer
+	else:
+		jump_buffer_countdown -= delta
 
-##### Attack Function #####
-func handle_attack(delta):
+
+func countdown_coyote(delta): # Counts down the coyote_countdown variable.
+	if is_on_floor():
+		coyote_countdown = coyote_time
+	else:
+		coyote_countdown -= delta
+
+
+func handle_jump(): # Responsible for jump and double jump mechanics.
+	if suspend_movement == false:
+		if is_on_floor():
+			jump_counter = 0
+			
+		if coyote_countdown > 0 and jump_buffer_countdown > 0:
+			velocity.y = jump_velocity
+			jump_buffer_countdown = 0
+		elif coyote_countdown < 0 and Input.is_action_just_pressed("Jump") and jump_counter < max_jumps - 1:
+			velocity.y = jump_velocity
+			jump_counter += 1
+			jump_buffer_countdown = 0
+			player_sprite.modulate = Color(0, 1, 0,) #Below 3 lines are to see when double jump occurs (as we dont have anim yet)
+			await get_tree().create_timer(0.3).timeout
+			player_sprite.modulate = Color(1, 1, 1)
+			
+		if not Input.is_action_pressed("Jump") and velocity.y < 0:
+			velocity.y = lerp(velocity.y, 0.0, 0.3)
+			
+		if Input.is_action_just_released("Jump"):
+			coyote_countdown = 0
+
+
+func handle_gravity(delta): # Controls gravities.
+	if velocity.y < 0:
+		velocity.y += jump_gravity * delta
+	elif velocity.y >= 0 and velocity.y < max_fall_speed:
+		velocity.y += descend_gravity * delta
+
+
+
+##### Health functions #####
+func take_damage(damage, hitbox_position, knockback_speed):
+	if invinc_timer <= 0:
+		if is_countering == false:
+			Global.player_take_damage(damage)
+			invinc_timer = 0.5
+			suspend_movement_timer = 0.1
+			suspend_movement = true
+			
+			var knockback_direction: Vector2 = (global_position - Vector2(0, 90)) - hitbox_position
+			velocity = Vector2(0, 0)
+			velocity = knockback_direction.normalized() * knockback_speed
+			
+			for i in range(3):
+				player_sprite.modulate = Color(0.8, 0.8, 0.8, 0.5)
+				await get_tree().create_timer(0.083334).timeout
+				player_sprite.modulate = Color(1, 1, 1, 1)
+				await get_tree().create_timer(0.083334).timeout
+			
+		elif is_countering == true:
+			can_counter = true
+			$CPUParticles2D.emitting = true # placeholder for anim
+			invinc_timer = 1.5
+			
+			for i in range(9):
+				player_sprite.modulate = Color(0.8, 0.8, 0.8, 0.5)
+				await get_tree().create_timer(0.083334).timeout
+				player_sprite.modulate = Color(1, 1, 1, 1)
+				await get_tree().create_timer(0.083334).timeout
+	else:
+		pass
+
+
+func handle_damage_timers(delta):
+	invinc_timer -= delta
+	suspend_movement_timer -= delta
+	if suspend_movement_timer <= 0:
+			suspend_movement = false
+
+
+
+##### Attack functions #####
+func handle_attacks():
 	if can_attack and Input.is_action_just_pressed("Attack_1"):
 		emit_signal("Attack1")
-		# Start punch
-		attack_active = true
-		can_attack = false
-		attack_timer = punch_speed
 
 	if can_attack and Input.is_action_just_pressed("Attack_2"):
 		emit_signal("Attack2")
@@ -280,72 +301,41 @@ func handle_attack(delta):
 	if can_attack and Input.is_action_just_pressed("Attack_4"):
 		emit_signal("Attack4")
 
-	if attack_active:
-		attack_timer -= delta
-		if attack_timer <= 0:
-			# End punch
-			attack_active = false
-			player_sprite.modulate = Color(1, 1, 1)  # Reset color
-			can_attack = true
 
 
-##### Parry and Counter Functions #####
-func handle_parry_counter(delta):
-	if Input.is_action_just_pressed("Attack_1") and not parry_active:
-		parry_active = true
-		#player_sprite.modulate = Color(0, 1, 1)  # Visual indicator of parry
-		await get_tree().create_timer(parry_window).timeout
-		parry_active = false
-		player_sprite.modulate = Color(1, 1, 1)  # Reset color after parry window ends
-
-	if Input.is_action_just_pressed("Counter") and not counter_active:
-		counter_active = true
-		player_sprite.modulate = Color(1, 1, 0)  # Visual indicator of counter
-		await get_tree().create_timer(counter_window).timeout
-		counter_active = false
-		player_sprite.modulate = Color(1, 1, 1)  # Reset color after counter window ends
-
-
-##### Attack-Detection Function #####
-func detect_attack():
-	# Check if player successfully parried
-	if parry_active:
-		# Add logic to prevent attack damage and refresh attacks
-		print("Parry successful!")
-		can_attack = true
-
-	elif counter_active:
-		# Add logic to prevent damage, refresh dashes, and unlock powerful moves
-		print("Counter successful!")
-		can_attack = true
-
-
-##### Health functions #####
-func take_damage(damage, hitbox_position, knockback_speed):
-	if invinc_timer <= 0:
-		Global.player_take_damage(damage)
-		invinc_timer = 0.5
-		suspend_movement_timer = 0.1
-		suspend_movement = true
-		
-		var knockback_direction: Vector2 = (global_position - Vector2(0, 90)) - hitbox_position
-		velocity = Vector2(0, 0)
-		velocity = knockback_direction.normalized() * knockback_speed
-		
-		for i in range(3):
-			player_sprite.modulate = Color(0.8, 0.8, 0.8, 0.5)
-			await get_tree().create_timer(0.083334).timeout
-			player_sprite.modulate = Color(1, 1, 1, 1)
-			await get_tree().create_timer(0.083334).timeout
+##### Counter and parry functions #####
+func handle_counter_cooldowns(delta):
+	if counter_active_timer > -5:
+		counter_active_timer -= delta
 	else:
 		pass
+	
+	if counter_cooldown_timer > -5:
+		counter_cooldown_timer -= delta
+	else:
+		pass
+	
+	if counter_cooldown_timer <= 0:
+		can_counter = true
+	else:
+		can_counter = false
 
 
-func handle_damage_timers(delta):
-	invinc_timer -= delta
-	suspend_movement_timer -= delta
-	if suspend_movement_timer <= 0:
-			suspend_movement = false
+func handle_counter():
+	if Input.is_action_just_pressed("Counter") and can_counter == true:
+		player_sprite.modulate = Color(1, 0, 0) # placeholder for anim
+		is_countering = true
+		counter_active_timer = counter_duration
+		counter_cooldown_timer = counter_cooldown
+		can_counter = false
+		await get_tree().create_timer(counter_duration).timeout # placeholder for anim
+		player_sprite.modulate = Color(1, 1, 1) # placeholder for anim
+	else:
+		pass
+	
+	if counter_active_timer <= 0:
+		is_countering = false
+
 
 
 ##### Collect Function #####
--- stuff/Scripts/global.gd
+++ stuff/Scripts/global.gd
@@ -1,16 +1,26 @@
 extends Node
 
 
+##### Player direction variables #####
 var player_facing_direction: int = 1
 var player_movement_direction: float = 0.0
 
-##### Player health/combat variables #####
+##### Player health variables #####
 @export var player_max_health: int = 100
 @export var player_health: int = 100
+
+
 
 ##### Player health/combat functions #####
 func player_take_damage(damage):
 	player_health -= damage
 	print(player_health)
+
+
+func player_death():
+	if player_health <= 0:
+		print("You died!")
+
+
 
 # TODO: change inventory to global file
